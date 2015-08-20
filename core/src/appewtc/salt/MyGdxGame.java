6bffd8d597ae90843c15b5de40bc98b2eae4889f
chanitnart <chanit_salt@yahoo.com>
2015-08-20 15:52:44 +0700

    Coins Ver2

--- core/src/appewtc/salt/MyGdxGame.java
+++ core/src/appewtc/salt/MyGdxGame.java
@@ -2,16 +2,21 @@ package appewtc.salt;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	private SpriteBatch batch;
 	//private Texture img;
-	private Texture wallpaperTexture;
+	private Texture wallpaperTexture, cloudTexture;
 	private OrthographicCamera objOrthographicCamera;
+	private BitmapFont nameBitmapFont;
+	private int xCloudAnInt, yCloudAnInt = 600; //cloud size 263*192 pixels
+	private boolean cloudABoolean = true;
 
 	@Override
 	public void create () {
@@ -23,7 +28,13 @@ public class MyGdxGame extends ApplicationAdapter {
 		//Setup Wallpaper
 		wallpaperTexture = new Texture("minion-1366x768.jpg");
 
-		//img = new Texture("badlogic.jpg");
+		//Setup BitMapFont
+		nameBitmapFont = new BitmapFont();
+		nameBitmapFont.setColor(Color.NAVY);
+		nameBitmapFont.setScale(4);
+
+		//Setup Cloud
+		cloudTexture = new Texture("cloud.png");
 	}//Create ส่วนกำหนดค่า
 
 	@Override
@@ -36,14 +47,38 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.setProjectionMatrix(objOrthographicCamera.combined);
 
 		//เอาไว้วาดวัตถุ Object
-		batch.begin();
-		//batch.draw(img, 0, 0);
+		batch.begin();  //batch.draw(img, 0, 0);
 
 		//Drawable Wallpaper
 		batch.draw(wallpaperTexture, 0, 0);
 
-		//
+		//Drawable BitMapFont
+		nameBitmapFont.draw(batch, "C o i n s   P B R U", 400, 550);
+
+		//Drawable Cloud
+		batch.draw(cloudTexture, xCloudAnInt, yCloudAnInt);
 
 		batch.end();
+
+		//Move Cloud ไว้นอก Class
+		moveCloud();
+
+	}
+
+	private void moveCloud() {
+		if (cloudABoolean==true) {
+			if (xCloudAnInt < 937) {
+				xCloudAnInt += 100 * Gdx.graphics.getDeltaTime();
+			} else {
+				cloudABoolean = !cloudABoolean;
+			}
+		} else {
+			if (xCloudAnInt > 0) {
+				xCloudAnInt -= 100 * Gdx.graphics.getDeltaTime();
+			} else {
+				cloudABoolean = !cloudABoolean;
+			}
+		}
+
 	}
 }
