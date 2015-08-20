bc2c110bb0784d48f0b41467b5e07fcff4b82327
chanitnart <chanit_salt@yahoo.com>
2015-08-20 16:51:21 +0700

    End_Day

--- core/src/appewtc/salt/MyGdxGame.java
+++ core/src/appewtc/salt/MyGdxGame.java
@@ -2,21 +2,27 @@ package appewtc.salt;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class MyGdxGame extends ApplicationAdapter {
 	private SpriteBatch batch;
 	//private Texture img;
-	private Texture wallpaperTexture, cloudTexture;
+	private Texture wallpaperTexture, cloudTexture, pigTexture;
 	private OrthographicCamera objOrthographicCamera;
 	private BitmapFont nameBitmapFont;
 	private int xCloudAnInt, yCloudAnInt = 600; //cloud size 263*192 pixels
 	private boolean cloudABoolean = true;
+	private Rectangle pigRectangle; //ใช้เขียน Control
+	private Vector3 objVector3;
+	private Sound pigSound;
 
 	@Override
 	public void create () {
@@ -35,6 +41,20 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		//Setup Cloud
 		cloudTexture = new Texture("cloud.png");
+
+		//Setup Pig
+		pigTexture = new Texture("pig.png");
+
+		//Setup Rectangle
+		pigRectangle = new Rectangle();
+		pigRectangle.x = 600 - 32; //(1200/2) - (64/2)
+		pigRectangle.y = 100;
+		pigRectangle.width = 64;
+		pigRectangle.height = 64;
+
+		//Setup Pig Sound
+		pigSound = Gdx.audio.newSound(Gdx.files.internal("pig.wav"));
+
 	}//Create ส่วนกำหนดค่า
 
 	@Override
@@ -52,11 +72,17 @@ public class MyGdxGame extends ApplicationAdapter {
 		//Drawable Wallpaper
 		batch.draw(wallpaperTexture, 0, 0);
 
+		//Drawable Cloud
+		batch.draw(cloudTexture, xCloudAnInt, yCloudAnInt);
+
+		//Active When Touch Screen --> Active เมื่อกระแทกจอ
+		activeTouchScreen();
+
 		//Drawable BitMapFont
 		nameBitmapFont.draw(batch, "C o i n s   P B R U", 400, 550);
 
-		//Drawable Cloud
-		batch.draw(cloudTexture, xCloudAnInt, yCloudAnInt);
+		//Drawable Pig
+		batch.draw(pigTexture, pigRectangle.x, pigRectangle.y);
 
 		batch.end();
 
@@ -64,6 +90,33 @@ public class MyGdxGame extends ApplicationAdapter {
 		moveCloud();
 
 	}
+
+	private void activeTouchScreen() {
+
+		if (Gdx.input.isTouched()) { //เมื่อนิ้วโดนจอ Boolean = True
+
+			//Pig Sound Effect
+			pigSound.play();
+
+			objVector3 = new Vector3(); //Vector3 จะเก็บ position X,y ที่นิ้วสัมผัส
+			objVector3.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			if (objVector3.x < 1200 / 2) {
+				//pigRectangle.x -= 10;
+				if (pigRectangle.x < 0) {
+					pigRectangle.x = 0;
+				} else {
+					pigRectangle.x -= 10;
+				}//if
+			} else {
+				if (pigRectangle.x > 1200 - 64) {
+					pigRectangle.x = 1200 - 64;
+				} else {
+					pigRectangle.x += 10;
+				}  //if
+			}
+
+		}//if
+	}//ActiveTouchScreen
 
 	private void moveCloud() {
 		if (cloudABoolean==true) {
